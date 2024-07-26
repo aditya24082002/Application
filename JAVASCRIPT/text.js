16854929959bd56fc711597f3a219abaef59b4af
Simplilearn GitHub <adityakumar24082002@gmail.com>
2024-07-26 10:16:36 +0530

    application

--- JAVASCRIPT/text.js
+++ JAVASCRIPT/text.js
@@ -1,12 +1,14 @@
 function findGreatestNumber(){
     var a=100;
-    var b=10;
+    var b=5;
     var c=10;
     (a>b && a>c)? document.write(a + " is greatest Number") : (b>a && b>c) ? document.write(b + " is greatest Number") : (c>a && c>b)? document.write(c + " is greatest Number") :document.write(c + " is equal among any one of " + a + " and " + b);
 }
 
 function findVote()
 {
-    document.write("Webdeveloper");
+    var age=5;
+    (age>18)?document.write(age + " is largest age man") :document.write(age + " is smallest age man");
+
 }
 
