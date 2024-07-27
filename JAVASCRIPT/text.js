eb63fe214de7ef8faa2f223a8d7a9509b33b741f
Simplilearn GitHub <adityakumar24082002@gmail.com>
2024-07-27 12:25:26 +0530

    application

--- JAVASCRIPT/text.js
+++ JAVASCRIPT/text.js
@@ -1,14 +1,25 @@
-function findGreatestNumber(){
-    var a=100;
-    var b=5;
-    var c=10;
-    (a>b && a>c)? document.write(a + " is greatest Number") : (b>a && b>c) ? document.write(b + " is greatest Number") : (c>a && c>b)? document.write(c + " is greatest Number") :document.write(c + " is equal among any one of " + a + " and " + b);
-}
-
-function findVote()
-{
-    var age=5;
-    (age>18)?document.write(age + " is largest age man") :document.write(age + " is smallest age man");
+function hello(){
+    document.write("Hello every one body<br>");
+    document.write("Hello every one body");
 
 }
+hello();
+
+
+function hello1(a,b){
+document.write("<br>");
+document.write(a+b);
+document.write("<br>")
+}
+hello1(9,6);
+
+
+function hello2(){
+    document.write("Aditya Kumar");
+    document.write('<img src="image/2024.jpeg" alt="Aditya Kumar" style="width: 200px; height: 100px;">');
+document.write("Aditya Kumar");
+}
+
+
+
 
